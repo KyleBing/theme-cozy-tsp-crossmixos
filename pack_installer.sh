7264ca51d21ac9fa0c51373148b664c9d0bc1621
KyleBing <kylebing@163.com>
2025-03-13 16:40:52 +0800

    add pack shell script

---
+++ pack_installer.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+7z a -tzip CozyInstaller.zip ./CozyInstaller -xr!._* -xr!.DS_Store
---
+++ pack_theme_only.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+7z a -tzip CozyTheme.zip ./CozyInstaller/ThemeFiles/Themes/Cozy -xr!._* -xr!.DS_Store
