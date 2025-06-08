f5b9c4afbc21a24b2c5b33bdcabb9bea41c10294
KyleBing(MBP14) <kylebing@163.com>
2025-06-08 23:13:01 +0800

    fit to new crossmix

--- pack_installer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-7z a -tzip CozyInstaller.zip ./CozyInstaller -xr!._* -xr!.DS_Store
--- pack_theme_only.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-7z a -tzip CozyTheme.zip ./CozyInstaller/ThemeFiles/Themes/Cozy -xr!._* -xr!.DS_Store
